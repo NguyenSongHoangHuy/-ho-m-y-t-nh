fa349cd55c19271b973b4fef12455d8765e3d08f
NguyenSongHoangHuy <49484648+NguyenSongHoangHuy@users.noreply.github.com>
2019-11-27 20:14:38 +0700

    update

--- Fractal.js
+++ Fractal.js
@@ -5,7 +5,11 @@ function getDrawType() {
     return document.getElementById("drawtype").value;
 }
 function Draw(repeatTimes, drawtype) {
+    var t0 = performance.now();
     if (drawtype == 1) {
         SierpinskiTriangle([-0.8, 1, -0.8, -1, Math.sqrt(3) - 0.8, 0,], repeatTimes, drawtype)
     }
+    var t1 = performance.now();
+    document.getElementById("status").innerText = `Finished rendering in ${Math.round(parseFloat(t1 - t0) * 1000) / 1000} ms.`
 }
+
